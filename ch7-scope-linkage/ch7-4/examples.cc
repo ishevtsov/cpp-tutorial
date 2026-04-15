4937d8a7f07c4c9ad2ac039edb5fee81cc097952
Igor Shevtsov <shevtsov@google.com>
2026-04-15 14:05:41 +0100

    Introduction to global variables

---
+++ ch7-scope-linkage/ch7-4/examples.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int g_x{}; // global variable with external linkage
+
+namespace Foo
+{
+    int g_x{}; // global variable with external linkage, but in namespace Foo
+}
+
+void doSomething()
+{
+    g_x = 3; // local variable with automatic storage duration and no linkage
+    std::cout << g_x << std::endl;
+
+    Foo::g_x = 4;
+    std::cout << Foo::g_x << std::endl;
+}
+
+int main()
+{
+    doSomething();
+    std::cout << g_x << std::endl; // g_x is accessible here because it has external linkage
+
+    g_x = 5;
+    std::cout << g_x << std::endl; // g_x is accessible here because it has external linkage
+
+    Foo::g_x = 6;
+    std::cout << Foo::g_x << std::endl; // g_x in namespace
+
+    return 0;
+}
