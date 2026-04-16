baee172342701c6f3bad59d7994f1fd58a907353
Igor Shevtsov <shevtsov@google.com>
2026-04-16 17:24:34 +0100

    Sharing global constants across multiple files (using inline variables)

---
+++ ch7-scope-linkage/ch7-10/constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Define your own namespace to hold constants
+namespace constants
+{
+    // Global constants have internal linkage by default
+    inline constexpr double pi{3.14159};
+    inline constexpr double avogadro{6.0221413e23};
+    inline constexpr double myGravity{9.2};
+}
+
+#endif
