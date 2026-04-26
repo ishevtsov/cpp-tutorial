d24c7b9072ff6424318d3c24c7b7df45d7e0ce83
Igor Shevtsov <shevtsov@google.com>
2026-04-26 12:41:55 +0100

    Halts (exiting your program early)

---
+++ ch8-control-flow/ch8-12/examples.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib> // for std::exit()
+
+void cleanup()
+{
+    // code here to do any kind of cleanup required
+    std::cout << "cleanup!\n";
+}
+
+int main()
+{
+    // register cleanup() to be called automatically when std::exit() is called
+    std::atexit(cleanup);
+
+    std::cout << 1 << '\n';
+
+    std::exit(0); // terminate and return status code 0 to operating system
+
+    // The following statements never execute
+    std::cout << 2 << '\n';
+
+    return 0;
+}
